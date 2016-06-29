22ae6dd610a182f7addf467433ff9507f89bc256
diverger <diverger@live.cn>
2016-06-29 16:15:35 +0800

    Add new.

---
+++ loop/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] primes = {2,3,5,7};
+
+        for (int t: primes)
+        {
+            System.out.println(t);
+        }
+    }
+}
