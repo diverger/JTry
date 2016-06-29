0ba5d49b60b54f09f751e70803410b3253b3f524
diverger <diverger@live.cn>
2016-06-29 18:34:43 +0800

    Add new try.

---
+++ class_try/src/com/company/Animal.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by diverger on 2016/6/29.
+ */
+public class Animal {
+    void bark()
+    {
+        System.out.println("Woof-Woof");
+    }
+}
--- loop/src/com/company/Main.java
+++ loop/src/com/company/Main.java
@@ -10,5 +10,21 @@ public class Main {
         {
             System.out.println(t);
         }
+
+        int result = 0;
+
+        for (int i = 0; i < 5; ++i)
+        {
+            if (i == 3)
+            {
+                result += 10;
+            }
+            else
+            {
+                result += i;
+            }
+        }
+
+        System.out.println(result);
     }
 }
